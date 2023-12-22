942ffcf5c017d34c5e58ea4a8f7f0f8786a87697
Marek Samofal <marek.samofal@gmail.com>
2023-12-22 19:24:16 +0100

    docs: Sign Up form pt.1

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,39 +1,43 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const defaultFormFields = {
-    displayName: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
+	displayName: '',
+	email: '',
+	password: '',
+	confirmPassword: '',
 }
 
 const SignUpForm = () => {
-    const [formFields, setFormFields] = useState(defaultFormFields);
-    const { displaName, email, password, confirmPassword } = formFields;
+	const [formFields, setFormFields] = useState(defaultFormFields)
+	const { displayName, email, password, confirmPassword } = formFields
 
-    const handleChange = (event) => {
-        const { name } = event;
-    };
+    console.log(formFields);
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+
+        setFormFields({ ...formFields, [name]: value })
+	}
 
 	return (
 		<div>
 			<h1>Sign up with your email and password</h1>
 			<form onSubmit={() => {}}>
 				<label>Display Name</label>
-				<input type='text' required onChange={handleChange} name="displayName" value={displaName} />
+				<input type='text' required onChange={handleChange} name='displayName' value={displayName} />
 
 				<label>Email</label>
-				<input type='email' required onChange={handleChange} name="email" />
+				<input type='email' required onChange={handleChange} name='email' value={email} />
 
 				<label>Password</label>
-				<input type='password' required onChange={handleChange} name="password" />
+				<input type='password' required onChange={handleChange} name='password' value={password} />
 
 				<label>Confirm Password</label>
-				<input type='password' required onChange={handleChange} name="confirmPassword" />
+				<input type='password' required onChange={handleChange} name='confirmPassword' value={confirmPassword} />
 				<button type='submit'>Sign up</button>
 			</form>
 		</div>
 	)
 }
 
-export default SignUpForm;
+export default SignUpForm
