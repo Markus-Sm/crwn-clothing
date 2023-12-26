f67d851c19b59040e73bc9de47356c055a3f7dee
Marek Samofal <marek.samofal@gmail.com>
2023-12-26 16:19:45 +0100

    docs: Sign up form pt. 2

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,12 @@
 import { initializeApp } from 'firebase/app'
 
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { 
+    getAuth, 
+    signInWithRedirect, 
+    signInWithPopup, 
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword 
+} from 'firebase/auth'
 
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
@@ -27,7 +33,9 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async userAuth => {
+export const createUserDocumentFromAuth = async (userAuth) => {
+    if(!userAuth) return;
+
 	const userDocRef = doc(db, 'users', userAuth.uid)
 
 	const userSnapshot = await getDoc(userDocRef)
@@ -50,3 +58,9 @@ export const createUserDocumentFromAuth = async userAuth => {
 
 	return userDocRef
 }
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password);
+};
