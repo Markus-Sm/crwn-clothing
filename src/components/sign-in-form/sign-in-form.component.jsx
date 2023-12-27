34976a2a8c8d60ab02e19c06df76c6b9e567e825
Marek Samofal <marek.samofal@gmail.com>
2023-12-27 21:40:03 +0100

    Finishing Authentication Page

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 
 import FormInput from '../form-input/form-input.component'
-
 import Button from '../button/button.component'
 
-import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import {
+	signInWithGooglePopup,
+	createUserDocumentFromAuth,
+	signInAuthUserWithEmailAndPassword,
+} from '../../utils/firebase/firebase.utils'
 
 import './sign-in-form.styles.scss'
 
@@ -17,38 +20,59 @@ const SignInForm = () => {
 	const [formFields, setFormFields] = useState(defaultFormFields)
 	const { email, password } = formFields
 
-	console.log(formFields)
+	const resetFormFields = () => {
+		setFormFields(defaultFormFields)
+	}
 
-    const resetFormFields = () => {
-        setFormFields(defaultFormFields);
-    }
+	const signInWithGoogle = async () => {
+		const { user } = await signInWithGooglePopup()
+		await createUserDocumentFromAuth(user)
+	}
 
 	const handleSubmit = async event => {
 		event.preventDefault()
 
 		try {
-
-            resetFormFields();
-		} catch (error) {}
+			const response = await signInAuthUserWithEmailAndPassword(email, password)
+			console.log(response)
+			resetFormFields()
+		} catch (error) {
+			switch(error.code){
+				case 'auth/wrong-password':
+					alert('incorrect password for email');
+					break
+				case 'auth/user-not-found':
+					alert('no user associated with this email');
+					break;
+				case 'auth/invalid-credential':
+					alert('incorrect password');
+					break;
+				default:
+					console.log(error);	
+			}
+		}
 	}
 
-	const handleChange = (event) => {
+	const handleChange = event => {
 		const { name, value } = event.target
 
 		setFormFields({ ...formFields, [name]: value })
 	}
 
 	return (
-		<div>
-            <h2>Already have an account?</h2>
+		<div className='sign-in-container'>
+			<h2>Already have an account?</h2>
 			<span>Sign in with your email and password</span>
 			<form onSubmit={handleSubmit}>
+				<FormInput label='Email' type='email' required onChange={handleChange} name='email' value={email} />
 
-				<FormInput label="Email" type='email' required onChange={handleChange} name='email' value={email} />
-
-				<FormInput label="Password" type='password' required onChange={handleChange} name='password' value={password} />
-
-				<Button type='submit'>Sign In</Button>
+				<FormInput label='Password' type='password' required onChange={handleChange} name='password' value={password} />
+				<div className='buttons-container'>
+					<Button type='submit'>Sign In</Button>
+					<Button type='button' buttonType='google' onClick={signInWithGoogle}>
+						Google sign in
+					</Button>
+				</div>
 			</form>
 		</div>
 	)
